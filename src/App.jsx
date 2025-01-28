8a7084dbe9a5674e86c38e6b3f36d8ecb085f657
raj-koshta <rajkoshta.rk1@gmail.com>
2025-01-29 01:32:56 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Movie from './pages/Movie';
+import Contact from './pages/Contact';
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "react-router-v6-in-reactv19/",
+      element: <Home />
+    },
+    {
+      path:"react-router-v6-in-reactv19//about",
+      element: <About />
+    },
+    {
+      path:"react-router-v6-in-reactv19/movie",
+      element: <Movie />
+    },
+    {
+      path:"react-router-v6-in-reactv19/contact",
+      element: <Contact />
+    }
+  ]);
+
   return (
-    <h1>
-      Hello React Router
-    </h1>
+    <RouterProvider router={router} />
   )
 }
 
